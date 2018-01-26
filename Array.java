38851733354502efd0d892fbc11d5e7be41824d9
Your Name <1185067763@qq.com>
2018-01-26 15:20:53 +0800

    Reform the sourcecode

--- Array.java
+++ Array.java
@@ -3,8 +3,8 @@ public class Array {
 	
 	public static void main( String[] args )
 	{
-		int[] arr = new int[]{1,2,3,4,5,6,7};
-      System.out.println( Arrays.toString(arr) );
+	  int[] arr = new int[]{1,2,3,4,5,6,7};
+          System.out.println( Arrays.toString(arr) );
 	  int tem = 0;
 		for(int i = 0;i < arr.length/2 ; i++)
 		{
@@ -13,7 +13,7 @@ public class Array {
 			arr[arr.length - 1-i] = tem;
 			
 		}
-	System.out.println( Arrays.toString(arr) );
+	   System.out.println( Arrays.toString(arr) );
 	
 	
 }
